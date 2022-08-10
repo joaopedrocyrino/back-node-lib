23fbda754e6773ff6dc80a5ad4b842f30646fcf4
Joao Cyrino <joaopedrocancado@gmail.com>
2022-08-10 18:06:09 -0300

    handle snake case

--- src/entities/utils.ts
+++ src/entities/utils.ts
@@ -13,3 +13,12 @@ export const createBase = (): baseDTO => {
         isDeleted: false
     }
 }
+
+export const handleSnakeCase = (entity: { [k: string]: any }, snakeCases: Array<[string, string]>) => {
+    snakeCases.forEach(sc => {
+        if (entity[sc[0]]) {
+            entity[sc[1]] = entity[sc[0]]
+        }
+    })
+    return entity
+}
